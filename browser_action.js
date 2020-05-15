2790cfdb9ed112efcbf41516ca478995b1bf4ac0
geckour <geckour@gmail.com>
2020-05-16 03:16:41 +0900

    Toggle start/stop
    Fix state handling
    
    Refactor
    
    a

--- browser_action.js
+++ browser_action.js
@@ -1,5 +1,11 @@
-const startPip = () => {
-  chrome.runtime.sendMessage({}, (_response) => { });
+const togglePip = () => {
+  chrome.runtime.sendMessage({}, (response) => {
+    if (response.state) {
+      document.getElementById('togglePipButton').innerHTML ='Stop PiP!';
+    } else {
+      document.getElementById('togglePipButton').innerHTML = 'Start PiP!';
+    }
+  });
 }
 
-document.getElementById('startPipButton').onclick = startPip;
+document.getElementById('togglePipButton').onclick = togglePip;
