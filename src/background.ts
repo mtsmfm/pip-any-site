69357dd916550e6d3955b2c2524dfda91d957473
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2021-06-06 07:12:59 +0000

    Typescript

---
+++ src/background.ts
@@ -0,0 +1,68 @@
+let video: HTMLVideoElement | null = null;
+let pip = false;
+
+interface HTMLVideoElement {
+  requestPictureInPicture: () => Promise<any>;
+}
+
+interface Document {
+  exitPictureInPicture: () => Promise<any>;
+}
+
+const startPip = () => {
+  chrome.tabCapture.capture(
+    {
+      video: true,
+      videoConstraints: {
+        mandatory: {
+          minWidth: 50,
+          minHeight: 50,
+          maxWidth: 1920,
+          maxHeight: 1080,
+          minFrameRate: 10,
+          maxFrameRate: 60,
+        },
+      },
+    },
+    (stream) => {
+      if (stream) {
+        video = document.createElement("video");
+        video.srcObject = stream;
+
+        video.addEventListener("enterpictureinpicture", () => {
+          pip = true;
+        });
+
+        video.addEventListener("leavepictureinpicture", () => {
+          pip = false;
+          stream.getTracks().forEach((t) => t.stop());
+        });
+
+        video.addEventListener("loadedmetadata", () => {
+          video!.requestPictureInPicture();
+          video!.play();
+        });
+
+        stream.addEventListener("inactive", () => {
+          if (pip) {
+            document.exitPictureInPicture();
+            video = null;
+          }
+        });
+      }
+    }
+  );
+};
+
+const stopPip = () => {
+  document.exitPictureInPicture();
+  video = null;
+};
+
+chrome.browserAction.onClicked.addListener(() => {
+  if (pip) {
+    stopPip();
+  } else {
+    startPip();
+  }
+});
